7aa4fa190b3cc4e021b9e984d80859095f24eabc
yaowei <yaowei@evga.com>
2018-06-10 01:26:49 -0700

    more widgets

---
+++ 4-basic_widgets/file_dialog.py
@@ -0,0 +1,60 @@
+"""
+This widget is a file selector dialog. It enables the user to navigate through the
+file system and select a file to open or save. The dialog is invoked either
+through static functions or by call exec_() function on the dialog object.
+"""
+import sys
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
+class filedialogdemo(QWidget):
+    def __init__(self, parent=None):
+        super(filedialogdemo, self).__init__(parent)
+
+        layout = QVBoxLayout()
+
+        self.btn = QPushButton("QFileDialog static method demo")
+        self.btn.clicked.connect(self.getfile)
+        layout.addWidget(self.btn)
+
+        self.le = QLabel("Hello")
+        layout.addWidget(self.le)
+
+        self.btn1 = QPushButton("QFileDialog object")
+        self.btn1.clicked.connect(self.getfiles)
+        layout.addWidget(self.btn1)
+
+        self.contents = QTextEdit()
+        layout.addWidget(self.contents)
+
+        self.setLayout(layout)
+        self.setWindowTitle("File Diaglog demo")
+
+    def getfile(self):
+        fname = QFileDialog.getOpenFileName(self, "Open file",
+                "c:\\", "Image files (*.jpg *.gif *.png)")
+        self.le.setPixmap(QPixmap(fname))
+
+    def getfiles(self):
+        dlg = QFileDialog()
+        dlg.setFileMode(QFileDialog.AnyFile)
+        dlg.setFilter("Text files (*.txt)")
+        #filenames = QStringList()
+
+        if dlg.exec_():
+            filenames = dlg.selectedFiles()
+            f = open(filenames[0], "r")
+            with f:
+                data = f.read()
+                self.contents.setText(data)
+
+def main():
+    app = QApplication(sys.argv)
+    ex = filedialogdemo()
+    ex.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
+
+
